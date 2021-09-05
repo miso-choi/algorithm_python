7361f1dfc26059e7e9639fcd21a10f59a39cfe50
Miso CHOI <78155086+miso-choi@users.noreply.github.com>
2021-09-05 14:45:09 +0900

    Update 타겟넘버.py

--- Programmers/깊이,너비 우선탐색/타겟넘버.py
+++ Programmers/깊이,너비 우선탐색/타겟넘버.py
@@ -1,7 +1,7 @@
 def solution(numbers, target):
-    if (not numbers) and (target == 0) : # numbers가 비었을때 & target이 0일때
+    if (not numbers) and (target == 0) : # numbers가 비었을때 & target이 0일때 ->  target + (수의 조합) = 0  (OK)
         return 1
-    elif (not numbers): # numbers가 비었을 때 & target이 0이 아닐때
+    elif (not numbers): # numbers가 비었을 때 & target이 0이 아닐때  ->   (No)
         return 0
-    else:  # numbers가 비지 않았을 때 & target은 상관 X
+    else:  # numbers가 비지 않았을 때 & (target은 상관 X) ->  더 깊이 들어가기  (Continue)
         return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
