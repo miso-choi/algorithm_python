ebdf0346bb16de62be513e466c972970bd00b275
Miso CHOI <78155086+miso-choi@users.noreply.github.com>
2021-09-18 13:14:24 +0900

    [Programmers] 이분탐색1

--- Programmers/이분탐색/입국심사.py
+++ Programmers/이분탐색/입국심사.py
@@ -31,12 +31,8 @@ def solution(n,times):
 
 
 ## n == people이 됐을 때 while문 어떻게 탈출하는가?
-## -> 우선 if에 걸리니 end = mid - 1 해준 뒤, 다음에는 else로 가서 start = mid+1 해주면, 그 다음에는 while문의 조건에 걸려서 탈출함.
+## -> 우선 if에 걸리니 end = mid - 1 해준다. 다음에는 바로 while문 조건에 걸리거나, else로 가서 start = mid+1 해준 뒤 while문 조건에 걸려서 탈출함.
   
       
 ## n == people인 경우 말고도, 왜 n < people인 경우까지 포함해서 answer = mid를 하여 저장하는가??
 ## -> 예시) n:9, times:[3,5,2]의 경우, 심사시간 10분동안 10명까지 가능한게 맞지만, 문제에서 9명을 심사하는데 걸리는 시간 구하라 했으니. 9명도 10분인건 똑같아서..
-
-
-  
-
