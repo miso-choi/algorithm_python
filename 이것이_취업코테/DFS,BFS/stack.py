cf86d91615d80937d8f13f3f80c2aa2f8f6ac854
Miso CHOI <78155086+miso-choi@users.noreply.github.com>
2021-09-20 00:08:48 +0900

    DFS,BFS Ch.1 스택 예제

--- 이것이_취업코테/DFS,BFS/stack.py
+++ 이것이_취업코테/DFS,BFS/stack.py
@@ -1,7 +1,8 @@
 # 스택(stack)은 list로 구현
 stack = []
 
-# 삽입은 append, 삭제는 pop 사용
+# 삽입(5)-삽입(2)-삽입(3)-삽입(7)-삭제()-삽입(1)-삽입(4)-삭제()
+# 삽입은 append(), 삭제는 pop() 사용
 stack.append(5)
 stack.append(2)
 stack.append(3)
@@ -10,6 +11,7 @@ stack.pop()     # 후입선출
 stack.append(1)
 stack.append(4)
 stack.pop()
+
 
 # 최하단 원소부터 출력 (먼저 들어온 순)
 print(stack)
