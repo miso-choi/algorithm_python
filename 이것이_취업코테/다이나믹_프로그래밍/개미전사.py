1f9f8b5fbcd38023bc6a1056ca13219981df1be6
Miso CHOI <78155086+miso-choi@users.noreply.github.com>
2021-10-04 23:58:49 +0900

    DP Ch.2 개미전사

---
+++ 이것이_취업코테/다이나믹_프로그래밍/개미전사.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+# DP 테이블 초기화
+d = [0]*100
+
+# Bottom-Up DP 진행
+d[0] = arr[0]
+d[1] = max(arr[0],arr[1])
+
+# 왼쪽에서부터 식량 털음. 
+for i in range(2,n):
+    d[i] = max(d[i-1], d[i-2]+arr[i])
+
+# arr 전체 (idx:0부터 n-1까지) 다 살펴본 결과, 최대 식량 수
+print(d[n-1])
