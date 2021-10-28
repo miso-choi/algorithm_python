5acee3680da82b3b478ccbf5f55766c87888fd37
Miso CHOI <78155086+miso-choi@users.noreply.github.com>
2021-10-28 19:21:40 +0900

    정렬 Ch.4

--- 이것이_취업코테/정렬/두_배열의_원소_교체.py
+++ 이것이_취업코테/정렬/두_배열의_원소_교체.py
@@ -8,14 +8,10 @@ def solution():
     for i in range(K):
         if A[i] < B[i]:
             A[i],B[i] = B[i],A[i]
-        else:
-            break
+        else:  
+            break   # 정렬했기 때문에 A[i] >= B[i]이면 종료
 
-    result = 0
-    for a in A:
-        result += a
-
-    return result
+    return sum(A)  # 기본 라이브러리 사용
 
 if __name__ == "__main__":
     result = solution()
